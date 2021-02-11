28aa7db3bded82af38161b1e3136cf4c5473f815
DraganD <dragandomazet94@gmail.com>
2021-02-11 18:40:33 +0100

    Refaktorisanje organizacije fajlova

--- TrafficSigns/main.py
+++ TrafficSigns/main.py
@@ -15,46 +15,26 @@ import os
 import pandas as pd
 import random
 from keras.preprocessing.image import ImageDataGenerator
+import preprocesingImages as ppi
+import util as ut
+import Constants
 
-##################### KONSTANTE
-pathToData = "myData"
-labelsFileName = "labels.csv"
-batch_size = 50
-steps_per_epoch = 2000
-epochs_number = 20
-imageDimensions = (32, 32, 3)
-testRatio = 0.2
-validationRatio = 0.2
-#################################
-
-###################### importovanje slika iz foldera myData
+# importovanje slika iz foldera myData
 count = 0
 images = []
 classNo = []
-myList = os.listdir(pathToData)
+myList = os.listdir(Constants.PATH_TO_DATA)
 myListLen = len(myList)
-print("Total Classes Detected: ", myListLen)
-print("Importing classes...")
 
-for x in range(0, myListLen):
-    myPicList: List[str] = os.listdir("{0}/{1}".format(pathToData, str(count)))
-    for y in myPicList:
-        currImg = cv2.imread("{0}/{1}/{2}".format(pathToData, str(count), y))
-        images.append(currImg)
-        classNo.append(count)
-    ##print(str(count), end_=" ")
-    count += 1
+images, classNo = ut.readMyData(myListLen, Constants.PATH_TO_DATA)
 images = np.array(images)
 classNo = np.array(classNo)
 
-############################### Split Data
-X_train, X_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
-X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validationRatio)
+# Deljenje podataka na train/test/validation (X_train = niz slika za treniranje, y_train = odgovarajuci ID klasa)
+X_train, X_test, y_train, y_test = train_test_split(images, classNo, test_size=Constants.TEST_RATIO)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=Constants.VALIDATION_RATIO)
 
-# X_train = ARRAY OF IMAGES TO TRAIN
-# y_train = CORRESPONDING CLASS ID
-
-############################### TO CHECK IF NUMBER OF IMAGES MATCHES TO NUMBER OF LABELS FOR EACH DATA SET
+## provera da li se broj slika poklapa sa brojem labela za svaki data-set
 print("Data Shapes")
 print("Train", end="");
 print(X_train.shape, y_train.shape)
@@ -63,36 +43,16 @@ print(X_validation.shape, y_validation.shape)
 print("Test", end="");
 print(X_test.shape, y_test.shape)
 
-############################### READ CSV FILE
-data = pd.read_csv(labelsFileName)
+# ucitavanje labela
+data = ut.readCsv(Constants.LABELS_FILE_NAME)
 print("data shape ", data.shape, type(data))
 
-############################### PREPROCESSING THE IMAGES
-def grayscale(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return img
+# Iteriranje i preprocesiranje svih slika
+X_train = np.array(list(map(ppi.preprocessing, X_train)))
+X_validation = np.array(list(map(ppi.preprocessing, X_validation)))
+X_test = np.array(list(map(ppi.preprocessing, X_test)))
 
-def equalize(img):
-    img = cv2.equalizeHist(img)
-    return img
-
-def preprocessing(img):
-    img = grayscale(img)  # CONVERT TO GRAYSCALE
-    img = equalize(img)  # STANDARDIZE THE LIGHTING IN AN IMAGE
-    img = img / 255  # TO NORMALIZE VALUES BETWEEN 0 AND 1 INSTEAD OF 0 TO 255
-    return img
-
-X_train = np.array(list(map(preprocessing, X_train)))  # TO ITERATE AND PREPROCESS ALL IMAGES
-X_validation = np.array(list(map(preprocessing, X_validation)))
-X_test = np.array(list(map(preprocessing, X_test)))
-#cv2.imshow("GrayScale Images",
-          # X_train[random.randint(0, len(X_train) - 1)])  # TO CHECK IF THE TRAINING IS DONE PROPERLY
-
-############################### ADD A DEPTH OF 1
+# dodavanje depth-a 1
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
 X_validation = X_validation.reshape(X_validation.shape[0], X_validation.shape[1], X_validation.shape[2], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
-
-print(X_train)
-print(X_validation)
-print(X_test)
